6921d9bfa636bcc3c6d7797a861012cbd544b589
chainsawriot <chainsawtiney@gmail.com>
2022-11-07 15:07:31 +0100

    make `parse_status` return `tibble` only so that list of tibbles is possible
    I think we can't support vanilla data.frame because it would be quite nasty to
    work with those "one-to-many" fields, e.g. tags, mentions. It looks nicer for a
    tibble of statuses, i.e. a timeline, in the future.

--- tests/testthat/test-parse_status.R
+++ tests/testthat/test-parse_status.R
@@ -1,26 +1,33 @@
-empty <- data.frame(id = NA_character_, uri = NA_character_, created_at = NA_character_, content = NA_character_, visibility = NA_character_, sensitive = NA, spoiler_text = NA_character_, reblogs_count = 0, favourites_count = 0, replies_count = 0, url = NA_character_, in_reply_to_id = NA_character_, in_reply_to_account_id = NA_character_, language = NA_character_, text = NA_character_, application = I(list(list())), poll = I(list(list())), card = I(list(list())), account = I(list(list())), reblog = I(list(list())), media_attachments = I(list(list())), mentions = I(list(list())), tags = I(list(list())), emojis = I(list(list())), favourited = NA, reblogged = NA, muted = NA, bookmarked = NA, pinned = NA)
+empty <- tibble::tibble(id = NA_character_, uri = NA_character_, created_at = NA_character_, content = NA_character_, visibility = NA_character_, sensitive = NA, spoiler_text = NA_character_, reblogs_count = 0, favourites_count = 0, replies_count = 0, url = NA_character_, in_reply_to_id = NA_character_, in_reply_to_account_id = NA_character_, language = NA_character_, text = NA_character_, application = I(list(list())), poll = I(list(list())), card = I(list(list())), account = I(list(list())), reblog = I(list(list())), media_attachments = I(list(list())), mentions = I(list(list())), tags = I(list(list())), emojis = I(list(list())), favourited = NA, reblogged = NA, muted = NA, bookmarked = NA, pinned = NA)
 
 test_that("status is NULL", {
-  expect_equal(parse_status(NULL, as_tibble = FALSE), empty)
+  expect_equal(parse_status(NULL), empty)
   expect_true("tbl_df" %in% class(parse_status(NULL)))
 })
 
 test_that("simplest cases", {
   status <- readRDS("../testdata/status/not_reblog.RDS")
-  expect_error(parse_status(status, as_tibble = FALSE), NA)
-  expect_error(parse_status(status, as_tibble = TRUE), NA)
-  res <- parse_status(status, as_tibble = FALSE)
+  expect_error(parse_status(status), NA)
+  res <- parse_status(status)
   expect_equal(colnames(empty), colnames(res))
 })
 
 test_that("With media attachments", {
   status <- readRDS("../testdata/status/single_media_attachment.RDS")
-  expect_error(parse_status(status, as_tibble = FALSE), NA)
-  expect_error(parse_status(status, as_tibble = TRUE), NA)
-  res <- parse_status(status, as_tibble = FALSE)
+  expect_error(parse_status(status), NA)
+  res <- parse_status(status)
   expect_equal(colnames(empty), colnames(res))
   expect_equal(length(res$media_attachments), 1)
-  expect_equal(res$media_attachments[[1]]$type, "image")
+  expect_equal(res$media_attachments[[1]]$type[1], "image")
+  expect_equal(length(res$media_attachments[[1]]$type), 2)
+  expect_true("tbl_df" %in% class(res$media_attachments[[1]]))
+})
+
+test_that("With tags", {
+  status <- readRDS("../testdata/status/multi_tags.RDS")
+  expect_error(res <- parse_status(status), NA)
+  expect_equal(nrow(res$tags[[1]]), 4)
+  expect_true("tbl_df" %in% class(res$tags[[1]]))
 })
 
 test_that("With and without personal fields", {
