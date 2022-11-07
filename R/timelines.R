98ece4003905cfb153b0314e1d2fcf9dc7551b20
chainsawriot <chainsawtiney@gmail.com>
2022-11-07 08:54:28 +0100

    Add `instance` parameter, ref #11

--- R/timelines.R
+++ R/timelines.R
@@ -2,8 +2,13 @@
 ## https://docs.joinmastodon.org/methods/statuses/
 ## https://docs.joinmastodon.org/methods/timelines/
 
-make_get_request <- function(token, path, params, ...) {
-  url <- prepare_url(token$instance)
+make_get_request <- function(token, path, params, instance = NULL, ...) {
+  token <- check_token_rtoot(token)
+  if (is.null(instance)) {
+    url <- prepare_url(token$instance)
+  } else {
+    url <- prepare_url(instance)
+  }
   request_results <- httr::GET(httr::modify_url(url, path = path),
                                httr::add_headers(Authorization = paste('Bearer', token$bearer)),
                                query = params)
@@ -20,15 +25,15 @@ make_get_request <- function(token, path, params, ...) {
 #' Query the instance for a specific status
 #'
 #' @param id character, Local ID of a status in the database
+#' @param instance character, the server name of the instance where the status is located. If `NULL`, the same instance used to obtain the token is used.
 #' @inheritParams post_toot
 #' @return a status
 #' token <- create_bearer(get_client(instance = "social.tchncs.de"), type = "user")
 #' get_status(id = "109298295023649405", token = token)
 #' @export
-get_status <- function(id, token = NULL) {
-  token <- check_token_rtoot(token)
+get_status <- function(id, instance = NULL, token = NULL) {
   path <- paste0("/api/v1/statuses/", id)
-  make_get_request(token = token, path = path, params = list())
+  make_get_request(token = token, path = path, params = list(), instance = instance)
 }
 
 #' Get the public timeline
@@ -42,6 +47,7 @@ get_status <- function(id, token = NULL) {
 #' @param min_id character, Return results immediately newer than this id
 #' @param limit integer, Maximum number of results to return
 #' @inheritParams post_toot
+#' @inheritParams get_status
 #' @return a list of statuses
 #' @export
 #' @examples
@@ -50,8 +56,7 @@ get_status <- function(id, token = NULL) {
 #' @references
 #' https://docs.joinmastodon.org/methods/timelines/
 get_public_timeline <- function(local = FALSE, remote = FALSE, only_media = FALSE,
-                                max_id, since_id, min_id, limit = 20L, token = NULL) {
-  token <- check_token_rtoot(token)
+                                max_id, since_id, min_id, limit = 20L, instance = NULL, token = NULL) {
   params <- list(local = local, remote = remote, only_media = only_media, limit = limit)
   if (!missing(max_id)) {
     params$max_id <- max_id
@@ -62,7 +67,7 @@ get_public_timeline <- function(local = FALSE, remote = FALSE, only_media = FALS
   if (!missing(min_id)) {
     params$min_id <- min_id
   }
-  make_get_request(token = token, path = "/api/v1/timelines/public", params = params)
+  make_get_request(token = token, path = "/api/v1/timelines/public", params = params, instance = instance)
 }
 
 ## A helper function to flatten a status
