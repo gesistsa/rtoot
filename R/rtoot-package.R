abdbc632e5c1dde57fe9bc58cc2f19e198eccd88
schochastics <david@schochastics.net>
2022-11-18 15:45:23 +0100

    add package-level documentation (#89)

---
+++ R/rtoot-package.R
@@ -0,0 +1,20 @@
+#' rtoot: Collect Mastodon data from R
+
+#' @description
+#' rtoots provides a range of functions to interact with the Mastodon API.
+#' It has five main groups of functions:
+#'
+#' * `get_account_<type>()` to get account level data
+#' * `get_timeline_<type>()` to get series of statuses
+#' * `get_instance_<type>()` get information for Mastodon instances
+#' * `stream_<type>()` to stream statuses from different timeline
+#' * `post_<type>()` to push content to Mastodon from R
+#'
+#' (Some auxiliary functions do not follow this naming scheme)
+#'
+#'
+#'
+#' Get started by reading `vignette("rtoot")`. and browse all vignettes with `browseVignettes(package = "rtoot")`
+#'
+#' @keywords internal
+"_PACKAGE"
