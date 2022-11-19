c75d06abf21cf3f767b6cfb17d75efe3d1739b93
chainsawriot <chainsawtiney@gmail.com>
2022-11-19 18:07:10 +0100

    Check for `clipr` first

--- tests/testthat/setup-rtoot.R
+++ tests/testthat/setup-rtoot.R
@@ -5,3 +5,8 @@ invisible(
          dir = vcr::vcr_test_path("fixtures")
        ))
 vcr::check_cassette_names()
+
+
+##skip_msg <- "System clipboard is not available - skipping test."
+is_clipr_available <- clipr::clipr_available()
+##message("Is clipr available?: ", is_clipr_available)
