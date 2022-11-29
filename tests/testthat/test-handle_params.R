3480d64590fce9bb25a452f75f67eb930d344a21
chainsawriot <chainsawtiney@gmail.com>
2022-11-29 19:07:29 +0100

    add an integration test for `handle_params`

--- tests/testthat/test-handle_params.R
+++ tests/testthat/test-handle_params.R
@@ -24,3 +24,21 @@ test_that("handle_params", {
   expect_equal(x$since_id, 2)
   expect_equal(x$min_id, 3)
 })
+
+test_that("integration test", {
+  ## The old wiki example; it use max_id
+  vcr::use_cassette("handle_params_max_id", {
+  max_id <- NULL
+  output <- tibble::tibble()
+  for (i in 1:4) {
+    res <- get_timeline_hashtag(hashtag = "ichbinhanna", max_id = max_id,
+                                limit = 40, instance = "mastodon.social",
+                                anonymous = TRUE)
+    output <- rbind(res, output)
+    max_id <- res$id[res$created_at == min(res$created_at)]
+    Sys.sleep(1)
+  }
+  })
+  expect_equal(nrow(output), 160)
+  expect_equal(length(unique(output$id)), 160) ## all unique
+})
