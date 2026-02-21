5cab3abe07f11b8fcf70c6c0c0ce292503806d49
chainsawriot <chainsawtiney@gmail.com>
2026-02-21 07:17:35 -0800

    Fix #189 (#190)

--- tests/testthat/test-auth_verify.R
+++ tests/testthat/test-auth_verify.R
@@ -1,14 +1,26 @@
 test_that("verify_envvar (Good case)", {
   ## The cassette was created with a valid envvar
+  ## But here is a fake
+  withr::local_envvar(
+    RTOOT_DEFAULT_TOKEN = paste0(
+      paste0(rep("a", 43), collapse = ""),
+      ";user;fosstodon.org"
+    )
+  )
   vcr::use_cassette("envvar", {
-    expect_error(capture_message(verify_envvar()), NA)
+    expect_error(x <- capture_message(verify_envvar()), NA)
   })
+  expect_false("simpleError" %in% class(x))
 })
 
 test_that("verify_envvar (Good case), silent", {
   ## The cassette was created with a valid envvar
-  skip_on_ci()
-  skip_on_cran()
+  withr::local_envvar(
+    RTOOT_DEFAULT_TOKEN = paste0(
+      paste0(rep("a", 43), collapse = ""),
+      ";user;fosstodon.org"
+    )
+  )
   vcr::use_cassette("envvar_silent", {
     expect_silent(verify_envvar(verbose = FALSE))
   })
